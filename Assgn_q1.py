dad4dddaf1eab2cce69bd4fe7f886a39246df739
sonimahesh <sonimahesh235@gmail.com>
2025-06-26 14:11:03 +0530

    Add files via upload

---
+++ Assgn_q1.py
@@ -0,0 +1,9 @@
+#Basic Math Operation in Python
+
+a=int(input("Enter First Number : "))
+b=int(input("Enter Second Number : "))
+
+print("Addition : ", a + b )
+print("Substraction : ", a-b)
+print("Multiplication : ", a*b)
+print("Division : ", a/b)
---
+++ Assgn_q2.py
@@ -0,0 +1,5 @@
+#Create a Personalized Greeting
+fisrtName=input("Enter your first name : ")
+lastname=input("Enter your last name : ")
+
+print("Hello ,",fisrtName,lastname,"! Welcome to the Python program.")
